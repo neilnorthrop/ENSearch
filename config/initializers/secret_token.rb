453679f794e7576cd52baa637e58c7eb2db5311a
Neil Northrop <nnorthrop@gmail.com>
2013-07-23 13:40:41 -0700

    add rails framework

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EchoNest::Application.config.secret_key_base = '2feaf66f8ddeecdd3055be1adae013abbba716da884b863f05a153f07d85013d5b4cf1c5d19eeda44b9393c6a26735e417751cdaa8bb56173c68c711d25c043f'
