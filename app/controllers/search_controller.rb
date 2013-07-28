d721f8efeb2b8e0d1ad709d6468cca116f848d8c
Neil Northrop <nnorthrop@gmail.com>
2013-07-28 09:51:31 -0700

    modify a few files

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -8,7 +8,7 @@ class SearchController < ApplicationController
     begin
       @submit_parse_fetch = ENApiFetch.new(params[:en_search_field][:artist_or_song]).submit_response
       @submit_parse_fetch = XMLParse.new(@submit_parse_fetch).url[0]
-    rescue  ENSearchUrlError => e
+    rescue ENSearchUrlError => e
       redirect_to :root, :flash => { :error => 'Invalid URL' }
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130725043635) do
-
-  create_table "searches", force: true do |t|
-    t.string   "search"
-    t.string   "result"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
+ActiveRecord::Schema.define(version: 20130727222438) do
 
 end
